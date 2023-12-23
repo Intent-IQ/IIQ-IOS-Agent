c8abb6db3294c085ff825d7ab257668414c13c6a
jstreamer <streamer.android@gmail.com>
2023-12-23 16:41:26 +0200

    Clean 2

---
+++ IIQ-IOS-Agent/IIQ-IOS-Agent-Example/IIQ_IOS_Agent_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIQ_IOS_Agent_ExampleApp.swift
+//  IIQ-IOS-Agent-Example
+//
+//  Created by Julian Rassolov on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct IIQ_IOS_Agent_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
