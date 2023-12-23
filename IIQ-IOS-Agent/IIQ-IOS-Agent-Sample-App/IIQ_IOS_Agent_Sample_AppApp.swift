66051b835154718d25ecede5ec34580d360a652c
Julian <julian@intentiq.com>
2023-12-23 16:17:39 +0200

    Initial

---
+++ IIQ-IOS-Agent/IIQ-IOS-Agent-Sample-App/IIQ_IOS_Agent_Sample_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIQ_IOS_Agent_Sample_AppApp.swift
+//  IIQ-IOS-Agent-Sample-App
+//
+//  Created by Julian Rassolov on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct IIQ_IOS_Agent_Sample_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
